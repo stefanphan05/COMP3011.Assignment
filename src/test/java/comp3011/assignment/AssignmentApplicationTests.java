fe33611239bfaa66b14d46bfe31a27a8b56e8528
a2966607 <a2966607@adelaide.edu.au>
2026-08-24 18:53:42 +0930

    first commit

---
+++ src/test/java/comp3011/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3011.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
